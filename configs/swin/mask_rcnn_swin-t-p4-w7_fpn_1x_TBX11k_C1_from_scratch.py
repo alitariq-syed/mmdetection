376fca50bb57081a427927cf3bc5b73b0ee85cd5
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2022-04-20 13:01:39 +0500

    from_scratch_test

---
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_1x_TBX11k_C1_from_scratch.py
@@ -0,0 +1,58 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/TBX11K_tb_only.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+#pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
+#train using swin-t classification model weights (weight sharing)
+#pretrained = '../../../mmclassification/tutorial_swin_C1/epoch_300.pth'
+
+#for neck and other output layers
+#load_path ='https://download.openmmlab.com/mmdetection/v2.0/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_20210906_131725-bacf6f7b.pth'
+
+model = dict(
+    type='FasterRCNN',
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        #init_cfg=dict(type='Pretrained', checkpoint=pretrained)
+        ),
+    neck=dict(in_channels=[96, 192, 384, 768]))
+   # 		init_cfg=dict(type='Pretrained', checkpoint=load_path)),
+   # rpn_head=dict(init_cfg=dict(type='Pretrained', checkpoint=load_path)),
+   # roi_head=dict(init_cfg=dict(type='Pretrained', checkpoint=load_path))
+   # )
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0005,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=0,
+    warmup='linear',
+    warmup_iters=250,
+    warmup_ratio=0.001)
+runner = dict(max_epochs=300)
